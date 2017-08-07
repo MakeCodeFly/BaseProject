53abc5c7cb94b03b71da780fa845a582caf365f9
zoujuequn <151315539@qq.com>
2017-08-07 12:27:56 +0800

    1修改Glide4.0

--- app/src/main/java/com/zoujuequn/baseproject/utils/GlideUtils.java
+++ app/src/main/java/com/zoujuequn/baseproject/utils/GlideUtils.java
@@ -7,6 +7,7 @@ import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.bumptech.glide.request.target.BitmapImageViewTarget;
 import com.zoujuequn.baseproject.R;
 
@@ -22,32 +23,37 @@ public class GlideUtils {
 
 
     public static void loadViewHolder(Activity mContext, String path, ImageView mImageView) {
-            Glide.with(mContext).load(path)
-                    .placeholder(R.mipmap.ic_launcher)//指定加载前显示的图片资源
-                    .error(R.mipmap.ic_launcher)//指定加载失败显示的图片资源
-                    .fallback(R.mipmap.ic_launcher)//指定传递加载资源为 null 的时候，显示的图片资源
-                    .into(mImageView);
+        RequestOptions requestOptions = new RequestOptions()
+                .placeholder(R.mipmap.ic_launcher)  //指定加载前显示的图片资源
+                .fallback(R.mipmap.ic_launcher)//指定传递加载资源为 null 的时候，显示的图片资源
+                .error(R.mipmap.ic_launcher);//指定加载失败显示的图片资源
+        Glide.with(mContext).load(path).apply(requestOptions).into(mImageView);
     }
+
     /**
      * 加载圆形头像
      */
     public static void loadCustomerViewHolder(final Activity activity, String path, final ImageView mImageView) {
-            Glide.with(activity).load(path).asBitmap().placeholder(R.mipmap.ic_launcher).error(R.mipmap.ic_launcher).
-                    fallback(R.mipmap.ic_launcher).
-                    into(new BitmapImageViewTarget(mImageView) {
-                @Override
-                protected void setResource(Bitmap resource) {
-                    RoundedBitmapDrawable circularBitmapDrawable =
-                            RoundedBitmapDrawableFactory.create(activity.getResources(), resource);
-                    circularBitmapDrawable.setCircular(true);
-                    mImageView.setImageDrawable(circularBitmapDrawable);
-                }
-            });
+        RequestOptions requestOptions = new RequestOptions()
+                .placeholder(R.mipmap.ic_launcher)  //指定加载前显示的图片资源
+                .fallback(R.mipmap.ic_launcher)//指定传递加载资源为 null 的时候，显示的图片资源
+                .error(R.mipmap.ic_launcher)//指定加载失败显示的图片资源
+                .transform(new GlideRoundTransform());
+        Glide.with(activity).load(path).apply(requestOptions).into(mImageView);
     }
 
 
-
-
+    /**
+     * 加载圆角头像
+     */
+    public static void loadRoundViewHolder(final Activity activity, String path, final ImageView mImageView) {
+        RequestOptions requestOptions = new RequestOptions()
+                .placeholder(R.mipmap.ic_launcher)  //指定加载前显示的图片资源
+                .fallback(R.mipmap.ic_launcher)//指定传递加载资源为 null 的时候，显示的图片资源
+                .error(R.mipmap.ic_launcher)//指定加载失败显示的图片资源
+                .transform(new GlideRoundTransform());
+        Glide.with(activity).load(path).apply(requestOptions).into(mImageView);
+    }
 
 }
 
